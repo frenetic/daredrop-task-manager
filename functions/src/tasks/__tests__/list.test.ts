ff2396c48fe161dc041ff0d9a32e621e0c182d69
Guilherme Medeiros <guicaraciolo@gmail.com>
2023-02-19 20:09:42 -0300

    fix breaking tests for LIST after test refactoring

--- functions/src/tasks/__tests__/list.test.ts
+++ functions/src/tasks/__tests__/list.test.ts
@@ -3,51 +3,17 @@ import { database } from "../../services/firebase";
 
 
 describe('Listing Tasks', () => {
-  beforeAll(async () => {
-    const tasks = [
-      {
-        name: 'test task',
-        description: 'first test task',
-        isDone: false,
-      },
-      {
-        name: 'second task',
-        description: 'second test task',
-        isDone: true,
-      },
-      {
-        name: 'last one',
-        description: 'it is boring to add tasks',
-        isDone: false,
-      }
-    ];
-
-    await Promise.all(
-      tasks.map(async (task) => {
-        const entry = await database.collection('tasks').doc();
-        await entry.set({ ...task, id: entry.id });
-      }),
-    );
-});
-
-afterAll(async () => {
-  const documents = await database.collection('tasks').listDocuments();
-  await Promise.all(
-    documents.map(async (doc) => await doc.delete()),
-  );
-});
-
-it('should list tasks', async () => {
-  const res = await req.get("/tasks").send();
-
-  expect(res.statusCode).toBe(200);
-  expect(res.body).toBeInstanceOf(Array);
-  expect(res.body.length).toBe(3);
-
-  const tasks = await database.collection('tasks').get();
-  const allTasks: any[] = [];
-  tasks.forEach(task => allTasks.push(task.data()));
-
-  expect(res.body).toStrictEqual(allTasks);
-});
+  it('should list tasks', async () => {
+    const res = await req.get("/tasks").send();
+  
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toBeInstanceOf(Array);
+    expect(res.body.length).toBe(2);
+  
+    const tasks = await database.collection('tasks').get();
+    const allTasks: any[] = [];
+    tasks.forEach(task => allTasks.push(task.data()));
+  
+    expect(res.body).toStrictEqual(allTasks);
+  });
 });
