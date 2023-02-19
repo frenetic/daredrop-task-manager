71b8cb1a248a32f9a5edc52e87bcece7d0209a3f
Guilherme Medeiros <guicaraciolo@gmail.com>
2023-02-19 16:31:38 -0300

    enabling schema validation to tasks creation

--- functions/src/__tests__/tasks.test.ts
+++ functions/src/__tests__/tasks.test.ts
@@ -24,4 +24,82 @@ describe('Creating Tasks', () => {
 
     expect(res.body).toStrictEqual(taskFromDB.data());
   });
+
+  it("should not create a task when json is empty", async () => {
+    const newTask = {};
+
+    const res = await req.post("/tasks").send(newTask);
+
+    expect(res.statusCode).toBe(400);
+  });
+
+  it("should not create a task when task is missing name", async () => {
+    const newTask = {
+      description: "Task without a name",
+      isDone: false,
+    };
+
+    const res = await req.post("/tasks").send(newTask);
+
+    expect(res.statusCode).toBe(400);
+    expect(res.body[0].message).toBe("must have required property 'name'");
+  });
+
+  it("should not create a task when json is empty", async () => {
+    const newTask = {};
+
+    const res = await req.post("/tasks").send(newTask);
+
+    expect(res.statusCode).toBe(400);
+  });
+
+  it("should not create a task when task is missing description", async () => {
+    const newTask = {
+      name: "Task without a description",
+      isDone: false,
+    };
+
+    const res = await req.post("/tasks").send(newTask);
+
+    expect(res.statusCode).toBe(400);
+    expect(res.body[0].message).toBe("must have required property 'description'");
+  });
+
+  it("should not create a task when task is missing isDone", async () => {
+    const newTask = {
+      name: "isDoneLess",
+      description: "Task without a isDone",
+    };
+
+    const res = await req.post("/tasks").send(newTask);
+
+    expect(res.statusCode).toBe(400);
+    expect(res.body[0].message).toBe("must have required property 'isDone'");
+  });
+
+  it("should not create a task when isDone is a string", async () => {
+    const newTask = {
+      name: "isDone is not boolean",
+      description: "isDone is a string",
+      isDone: "true",
+    };
+
+    const res = await req.post("/tasks").send(newTask);
+
+    expect(res.statusCode).toBe(400);
+    expect(res.body[0].message).toBe("must be boolean");
+  });
+
+  it("should not create a task when isDone is an integer", async () => {
+    const newTask = {
+      name: "isDone is not boolean",
+      description: "isDone is am integer",
+      isDone: 1,
+    };
+
+    const res = await req.post("/tasks").send(newTask);
+
+    expect(res.statusCode).toBe(400);
+    expect(res.body[0].message).toBe("must be boolean");
+  });
 });
--- functions/src/tasks.ts
+++ functions/src/tasks.ts
@@ -1,9 +1,30 @@
+import Ajv from 'ajv';
 import * as express from 'express';
 import { database } from './services/firebase';
 
 const tasksRouter = express.Router();
 
 tasksRouter.post('/tasks', async function(req, res) {
+  const schema = {
+    type: "object",
+    properties: {
+      name: {type: "string"},
+      description: {type: "string"},
+      isDone: {type: "boolean"}
+    },
+    required: ["name", "description", "isDone"],
+    additionalProperties: false
+  }
+  const ajv = new Ajv();
+  
+  const validate = ajv.compile(schema);
+
+  const isValid = validate(req.body);
+  if (!isValid) {
+    res.status(400).send(validate.errors);
+    return;
+  }
+
   const task = req.body;
 
   const entry = await database.collection('tasks').doc();
