4f548b843265e1e6737863f1942961c6b7189024
Guilherme Medeiros <guicaraciolo@gmail.com>
2023-02-19 03:12:37 -0300

    replacing default example with express

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,10 @@
 import * as functions from "firebase-functions";
+import * as express from 'express';
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+const app = express();
+app.get('/', (request, response) => {
+    functions.logger.info("Hello logs!", {structuredData: true});
+    response.send("Hello from Firebase!");
+})
+exports.app = functions.https.onRequest(app)
