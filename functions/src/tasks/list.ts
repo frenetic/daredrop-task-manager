cc18542f52b815734d6ecd6686b7054111db0751
Guilherme Medeiros <guicaraciolo@gmail.com>
2023-02-19 17:28:17 -0300

    create a Task type for listing to remove forced any[]

--- functions/src/tasks/list.ts
+++ functions/src/tasks/list.ts
@@ -1,12 +1,18 @@
 import { Request, Response } from 'express';
 import { database } from '../services/firebase';
 
+type Task = {
+  name: string;
+  description: string;
+  isDone: boolean;
+};
+
 export async function list (req: Request, res: Response) {
   const tasks = await database.collection('tasks').get();
 
-  const allTasks: any[] = [];
+  const allTasks: Task[] = [];
 
-  tasks.forEach(task => allTasks.push(task.data()));
+  tasks.forEach(task => allTasks.push(task.data() as Task));
 
   res.status(200).send(allTasks);
 }
