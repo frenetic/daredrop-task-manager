947692ba55b24d524f32f1819f0f6e2631d75a60
Guilherme Medeiros <guicaraciolo@gmail.com>
2023-02-19 21:28:26 -0300

    enable filtering list by isDone

--- functions/src/tasks/__tests__/list.test.ts
+++ functions/src/tasks/__tests__/list.test.ts
@@ -3,17 +3,49 @@ import { database } from "../../services/firebase";
 
 
 describe('Listing Tasks', () => {
-  it('should list tasks', async () => {
+  it('should list all tasks', async () => {
     const res = await req.get("/tasks").send();
-  
+
     expect(res.statusCode).toBe(200);
     expect(res.body).toBeInstanceOf(Array);
     expect(res.body.length).toBe(2);
-  
+
     const tasks = await database.collection('tasks').get();
     const allTasks: any[] = [];
     tasks.forEach(task => allTasks.push(task.data()));
-  
+
     expect(res.body).toStrictEqual(allTasks);
+  });
+
+  it('should list filter by isDone = true', async () => {
+    const res = await req.get("/tasks?isDone=true").send();
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toBeInstanceOf(Array);
+    expect(res.body.length).toBe(1);
+
+    expect(res.body).toStrictEqual([
+      {
+        name: 'ignore this other one as well',
+        description: 'this task will also be ignored',
+        isDone: true,
+      }
+    ]);
+  });
+
+  it('should list filter by isDone = false', async () => {
+    const res = await req.get("/tasks?isDone=false").send();
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toBeInstanceOf(Array);
+    expect(res.body.length).toBe(1);
+
+    expect(res.body).toStrictEqual([
+      {
+        name: 'ignore this task',
+        description: 'this task will be ignore',
+        isDone: false,
+      }
+    ]);
   });
 });
--- functions/src/tasks/list.ts
+++ functions/src/tasks/list.ts
@@ -7,12 +7,35 @@ type Task = {
   isDone: boolean;
 };
 
-export async function list (req: Request, res: Response) {
+async function listAll(req: Request, res: Response) {
   const tasks = await database.collection('tasks').get();
 
   const allTasks: Task[] = [];
 
   tasks.forEach(task => allTasks.push(task.data() as Task));
 
-  res.status(200).send(allTasks);
+  return res.status(200).send(allTasks);
+}
+
+async function filterList(req: Request, res: Response) {
+  let query = database.collection('tasks');
+
+  if (req.query.hasOwnProperty('isDone')) {
+    query = query.where('isDone', '==', req.query.isDone === 'true') as any;
+  }
+
+  const results = await query.get();
+
+  const allTasks: Task[] = [];
+  results.forEach(task => allTasks.push(task.data() as Task));
+
+  return res.status(200).send(allTasks);
+}
+
+export async function list(req: Request, res: Response) {
+  if (!req.query) {
+    return listAll(req, res);
+  }
+
+  return filterList(req, res);
 }
